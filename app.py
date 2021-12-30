7d3ff6ae5a07099443b4991c8e8c2b736529ea4d
wowba <87873821+wowba@users.noreply.github.com>
2021-12-30 23:02:47 +0900

    study

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 from dotenv import load_dotenv
 import os
@@ -8,10 +8,38 @@ app = Flask(__name__)
 load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 
+from pymongo import MongoClient
+
+# localhost 용 MongoDB 연결
+client = MongoClient("localhost", 27017)
+
+# 서버용 MongoDB 연결
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+
+db = client.mapCrawlingPrac
+addresses = []
+
+# html 렌더링
 @app.route("/")
 def home():
     return render_template("index.html", CLIENT_ID=CLIENT_ID)
 
+# api
+
+# 데이터 가져오기
+#도봉구
+@app.route("/map/dobong", methods=["GET"])
+def dobong():
+    shopdata = list(db.shops.find({},{"_id":False}))
+
+    return jsonify({"shopdatas":shopdata})
+
+#노원구
+@app.route("/map/nowon", methods=["GET"])
+def nowon():
+    shopdata = list(db.shops.find({},{"_id":False}))
+
+    return jsonify({"shopdatas":shopdata})
 
 # Flask 시작
 
